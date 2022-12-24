531d9f09e4381395796da55f595224045750e452
Bruno Ribeiro <brf147@gmail.com>
2022-12-24 16:21:07 -0400

    My Vim configuration

---
+++ after/plugin/mason.rc.lua
@@ -0,0 +1,9 @@
+local status, mason = pcall(require, 'mason')
+if (not status) then return end
+local status, lspconfig = pcall(require, 'mason-lspconfig')
+if (not status) then return end
+
+mason.setup {}
+lspconfig.setup {
+  ensure_installed = { 'tailwindcss', 'elixirls', 'dockerls' }
+}
